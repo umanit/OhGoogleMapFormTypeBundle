3db5b391599ecc7423183e42670b0d6085340efa
Nicolas Panau <npanau@umanit.fr>
2017-07-18 10:56:23 +0200

    apikey

--- DependencyInjection/OhGoogleMapFormTypeExtension.php
+++ DependencyInjection/OhGoogleMapFormTypeExtension.php
@@ -21,8 +21,10 @@ class OhGoogleMapFormTypeExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('twig.xml');
         $loader->load('services.xml');
+
+        $container->setParameter('api_key', $config['api_key']);
     }
 }
